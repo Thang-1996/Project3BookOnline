447474fda9cc1998678fac220b3e50894ea6dd7e
Thang-1996 <minhthang009@gmail.com>
2021-01-14 12:40:09 +0700

    product controller

--- Controllers/api/ProductsController.cs
+++ Controllers/api/ProductsController.cs
@@ -35,7 +35,12 @@ namespace BookOnlineShop.Controllers.api
 
         public async Task<ActionResult<Products>> GetProducts(int id)
         {
-            var products = await _context.Products.FindAsync(id);
+            var products = await _context.Products
+                .Include(ap => ap.AuthorProducts)
+                .ThenInclude(a => a.AuthorID)
+                .Include(pp => pp.PublisherProducts)
+                .ThenInclude(p => p.PublisherID)
+                .Where(p => p.ProductID == id).FirstOrDefaultAsync();
 
             if (products == null)
             {
